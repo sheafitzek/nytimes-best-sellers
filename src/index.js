2af078a9a3b69d52cb2a7edf1f60cd6cf887540b
sheafitzek <agg817@sbcglobal.net>
2018-02-17 21:11:22 -0600

    add booklist component with dummy data & wire up to redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
 
-import App from './App';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById(`root`));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+render(
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<App />
+	</Provider>,
+	document.getElementById(`root`)
+);
+
 registerServiceWorker();
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{title: `JavaScript, the Good Parts`},
+		{title: `Harry Potter`},
+		{title: `Eloquent JavaScript`},
+		{title: `The Dark Tower`},
+	];
+}
